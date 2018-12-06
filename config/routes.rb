6908cfec6c95b88dcd20b71edd1e2b77bb4c4d8d
hasegawa-go <gou1982@gmail.com>
2018-12-06 12:54:25 +0900

    SNS認証gem追加
    フェイスブック、グーグル認証のgemを追加しました。

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  # devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'items#index'
+
+  devise_for :users, controllers: {
+    omniauth_callbacks: "users/omniauth_callbacks"
+}
+get "pages/user_info", as: "user_root"
 end
