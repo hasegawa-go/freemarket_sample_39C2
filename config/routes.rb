166faa5f2d1a7cc2a610c93899e8ced8f68d8212
chikako-LLB <chikakolilleboe@gmail.com>
2018-12-06 23:48:56 +0900

    undo changes to routing

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'items#index'
 
-
   devise_for :users, controllers: {
     omniauth_callbacks: "users/omniauth_callbacks"
-  }
-
-  get "pages/user_info", as: "user_root"
+}
+# get "pages/user_info", as: "user_root"
 
   get  'mypage', to: 'users#mypage', as: :user_mypage
 
+end
