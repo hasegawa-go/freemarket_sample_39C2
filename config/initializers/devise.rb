dc68013a47712e3db287eb3ce468182aad8c930f
hasegawa-go <gou1982@gmail.com>
2018-12-08 13:20:51 +0900

    SNS認証ID登録修正
    ・Facebook、Googleの各IDを環境変数へ設定
    
    ・gitignoreにsecrets.ymlを記述

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -3,7 +3,7 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
-  config.omniauth :facebook, '592408507862784', '564e9a9abe21af69ec05dda036f073ec'
+  config.omniauth :facebook,ENV['FACEBOOK']
 
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
