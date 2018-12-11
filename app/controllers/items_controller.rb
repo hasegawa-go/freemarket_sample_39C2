8957843c6e8db3976dc343ba47e8c37c7a479f50
chikako-LLB <chikakolilleboe@gmail.com>
2018-12-11 23:33:05 +0900

    adding views

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,28 @@
 class ItemsController < ApplicationController
-  # メッセージ表示用に仮置き
+  before_action :move_to_index, except: :index
+  
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+	 	# Item.create(image: item_params[:image], text: tweet_params[:text],user_id: current_user.id)
+  end
+
+  def show
+  end
+
+
+private
+    def item_params
+      params.permit(:image,:text,)
+    end
+
+    def move_to_index
+      redirect_to action: :index unless user_signed_in?
+    end
 end
+
