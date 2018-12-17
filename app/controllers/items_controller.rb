fdffece451ac3c28fbcd188530fbd56ebd7e8e3d
chikako-LLB <chikakolilleboe@gmail.com>
2018-12-17 14:07:28 +0900

    4times build images

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,7 +6,7 @@ class ItemsController < ApplicationController
 
   def new
     @item = Item.new
-    @item.images.build
+    4.times{@item.images.build}
   end
 
   def create
@@ -24,7 +24,7 @@ class ItemsController < ApplicationController
 
 private
     def item_params
-      params.require(:item).permit(:name, :description, :condition, :price, images_attributes: [:image] ).merge(seller_id: current_user.id)
+      params.require(:item).permit(:name, :description, :condition, :price, images_attributes:[:image] ).merge(seller_id: current_user.id)
 
     end
 
