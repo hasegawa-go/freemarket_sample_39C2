93a34a5bf2e842fb8e951cdb6f962ec36e3f1d76
chikako-LLB <chikakolilleboe@gmail.com>
2018-12-18 20:29:44 +0900

    preview

---
+++ app/assets/javascripts/item.js
@@ -0,0 +1,26 @@
+$(document).on('turbolinks:load', function(){
+	$(function previewFile (){
+		$('.upload-photo').on('change', function(e){
+			//イベントを発生させたオブジェクトを変数fileに入れる
+			var file = e.target.files[0];
+			console.log(file)
+			// FileReaderでfileオブジェクトの中身を取得してreaderに入れる
+			var reader = new FileReader();
+			console.log(reader)
+			//HTMLのimgタグのオブジェクトをpreviewに入れる
+			var preview  = $(".preview");
+			console.log(preview)
+
+
+
+ 			//オブジェクトを読み込んでから処理を実行する
+      reader.onload = (function (file) {
+      	return function(e){
+      		$(".preview").attr("src", e.target.result);
+      		$(".preview").attr("size", e.target.result);
+      	}
+			})(file);
+			reader.readAsDataURL(file);
+		})
+  })
+})
