d3bbf074cc2b51e136da6b7fc9505c99ed8589fd
chikako-LLB <chikakolilleboe@gmail.com>
2018-12-21 02:36:35 +0900

    プレビュー機能

--- app/assets/javascripts/item.js
+++ app/assets/javascripts/item.js
@@ -1,21 +1,20 @@
 $(document).on('turbolinks:load', function(){
-	// $(function previewFile (){
-		$('.upload-photo').on('change', function(changeEvent){
+	$('.upload-photo').on('change', function(changeEvent){
 
-			var file = changeEvent.target.files[0];
+		var file = changeEvent.target.files[0];
 
-			var reader = new FileReader();
+		var reader = new FileReader();
 
 
-		    reader.onload = (function (file) {
-		      	return function(onloadEvent){
-		      		var imageContainer = $(changeEvent.target).siblings().filter('.preview');
-		      		imageContainer.attr("src", onloadEvent.target.result);
-		      		imageContainer.attr({height:"100px;", width:"100px"}, onloadEvent.target.result);
-		      	}
-			})(file);
+	    reader.onload = (function (file) {
+	      	return function(onloadEvent){
+	      		var imageContainer = $(changeEvent.target).siblings().filter('.preview');
+	      		imageContainer.attr("src", onloadEvent.target.result);
+	      		imageContainer.attr({height:"100px;", width:"100px"}, onloadEvent.target.result);
+	      	}
+		})(file);
 
-			reader.readAsDataURL(file);
-		})
-  	})
-// })
+		reader.readAsDataURL(file);
+	})
+})
+
