67a71e4942efe3cc9c5dae6e526d34acee299e33
chikako-LLB <chikakolilleboe@gmail.com>
2018-12-20 13:06:25 +0900

    multiple → 4.times

--- app/assets/javascripts/item.js
+++ app/assets/javascripts/item.js
@@ -1,27 +1,21 @@
 $(document).on('turbolinks:load', function(){
-	$(function previewFile (){
-		$('.upload-photo').on('change', function(e){
-			//イベントを発生させたオブジェクトを変数fileに入れる
-			var file = e.target.files[0];
-			var fileLength = e.target.files.length;
-			console.log(fileLength)
-			// FileReaderでfileオブジェクトの中身を取得してreaderに入れる
+	// $(function previewFile (){
+		$('.upload-photo').on('change', function(changeEvent){
+
+			var file = changeEvent.target.files[0];
+
 			var reader = new FileReader();
-			// console.log(reader)
-			//HTMLのimgタグのオブジェクトをpreviewに入れる
-			var preview  = $(".preview");
-			// console.log(preview)
 
 
-
- 			//オブジェクトを読み込んでから処理を実行する
-      reader.onload = (function (file) {
-      	return function(e){
-      		$(".preview").attr("src", e.target.result);
-      		$(".preview").attr("size", e.target.result);
-      	}
+		    reader.onload = (function (file) {
+		      	return function(onloadEvent){
+		      		var imageContainer = $(changeEvent.target).siblings().filter('.preview');
+		      		imageContainer.attr("src", onloadEvent.target.result);
+		      		imageContainer.attr("size", onloadEvent.target.result);
+		      	}
 			})(file);
+
 			reader.readAsDataURL(file);
 		})
-  })
-})
+  	})
+// })
